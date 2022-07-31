3609478fa89b0023e6b2e086b5562686f9d0d8d4
alexflexxy <57566634+alexflexxy@users.noreply.github.com>
2022-07-31 13:57:28 +0100

    first commit

---
+++ chat_project/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+from chattoo import views
+
+urlpatterns = [
+    path('', views.home, name='home' ),
+    path('signup/', views.signup, name= 'signup'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('admin/', admin.site.urls),
+]
---
+++ chattoo/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ChattooConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'chattoo'
